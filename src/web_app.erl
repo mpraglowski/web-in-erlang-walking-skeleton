928c39c83417f792d1682a6f119413cedef6f6eb
Mirosław Pragłowski <m@praglowski.com>
2015-11-03 21:45:00 +0100

    Added Cowboy setup service static filed from priv folder

--- src/web_app.erl
+++ src/web_app.erl
@@ -16,6 +16,11 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([{'_', routes() }]),
+    Port = port(),
+    {ok, _} = cowboy:start_http(web_http_listener, 100, [{port, Port}],
+                                 [{env, [{dispatch, Dispatch}]}]
+                               ),
     'web_sup':start_link().
 
 %%--------------------------------------------------------------------
@@ -25,3 +30,16 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+port() ->
+  case os:getenv("PORT") of
+    false -> 8080;
+    Value -> list_to_integer(Value)
+  end.
+
+routes() ->
+  [
+    {"/",       cowboy_static, {priv_file, web, "index.html"}},
+    {"/[...]",  cowboy_static, {priv_dir,  web, "",
+                                    [{mimetypes, cow_mimetypes, all}]}}
+  ].
